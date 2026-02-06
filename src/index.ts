c17ca76cae99a3a5786fc035e4a87add56a22628
marianoInsa <marianoinsaurralde5@gmail.com>
2026-02-05 21:01:10 -0300

    add: endpoint GET productos

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,47 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { Hono } from 'hono';
+import { cors } from 'hono/cors';
 
-export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
-	},
-} satisfies ExportedHandler<Env>;
+// interfaz para la DB
+type Bindings = {
+	DB: D1Database;
+};
+
+const app = new Hono<{ Bindings: Bindings }>();
+
+app.use('/api/*', cors());
+
+// health check
+app.get('/', (c) => {
+	return c.json({
+		status: 'ok',
+		message: 'Laburen · MCP Server Activo',
+	});
+});
+
+// 1 - Explorar productos
+// GET /products
+app.get('/api/products', async (c) => {
+	try {
+		let query = 'SELECT * FROM products WHERE available = 1';
+		let params: any[] = [];
+
+		// limito a 5 resultados
+		query += ' LIMIT 5';
+
+		const { results } = await c.env.DB.prepare(query)
+			.bind(...params)
+			.all();
+
+		return c.json({ products: results });
+	} catch (error) {
+		return c.json(
+			{
+				error: 'Error al buscar productos',
+				details: String(error),
+			},
+			500,
+		);
+	}
+});
+
+export default app;
